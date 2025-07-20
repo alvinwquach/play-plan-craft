0be00430b36ab10463646e9c4a54fa18f61877c6
Alvin Quach <alvinwquach@gmail.com>
2025-07-19 18:00:48 -0700

    Remove unused imports

--- app/db/schema/relations/userRelations.ts
+++ app/db/schema/relations/userRelations.ts
@@ -1,14 +1,3 @@
-import {
-  pgTable,
-  text,
-  integer,
-  timestamp,
-  boolean,
-  index,
-  serial,
-  primaryKey,
-  varchar,
-} from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 import { lessonNotes } from "../table/lessonNotes";
 import { lessonPlans } from "../table/lessonPlans";
