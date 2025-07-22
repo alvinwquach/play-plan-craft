3b9442380a8dc2df23a2117ca09dc3b081c15c65
Alvin Quach <alvinwquach@gmail.com>
2025-07-21 18:30:09 -0700

    Fix activities relation

--- app/db/schema/relations/activitiesRelations.ts
+++ app/db/schema/relations/activitiesRelations.ts
@@ -5,15 +5,17 @@ import { developmentGoals } from "../table/developmentGoals";
 import { lessonPlansDevelopmentGoals } from "../table/lessonPlansDevelopmentGoals";
 
 // Activity relations: Defines how activities relate to lesson plans and development goals.
-export const activitiesRelations = relations(activities, ({ one }) => ({
+export const activitiesRelations = relations(activities, ({ one, many }) => ({
   // Many-to-one: One activity belongs to one lesson plan.
   lessonPlan: one(lessonPlans, {
     fields: [activities.lessonPlanId],
     references: [lessonPlans.id],
   }),
-  // Many-to-one: One activity is linked to one development goal via lesson plan.
-  developmentGoal: one(developmentGoals, {
+  // Many-to-many: One activity is linked to one or more development goals via lesson plan.
+  developmentGoals: many(developmentGoals, {
     fields: [lessonPlansDevelopmentGoals.developmentGoalId],
     references: [developmentGoals.id],
+    where: (lessonPlansDevelopmentGoals) =>
+      lessonPlansDevelopmentGoals.lessonPlanId.equals(activities.lessonPlanId),
   }),
 }));
